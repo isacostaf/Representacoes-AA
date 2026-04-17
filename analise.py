8159f1acdbe823c43b03b533bbb5b43a0964dac5
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2026-04-17 14:51:14 -0300

    att main

--- analise.py
+++ analise.py
@@ -227,14 +227,13 @@ def processar_link(item):
 # =========================
 def analisar_links(url_busca, palavras_usuario, status=None, progress=None):
     
-    options = webdriver.ChromeOptions()
-    options.binary_location = "/usr/bin/chromium"
+    options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
-        service=Service("/usr/bin/chromedriver"),
+        service=Service(ChromeDriverManager().install()),
         options=options
     )
     
